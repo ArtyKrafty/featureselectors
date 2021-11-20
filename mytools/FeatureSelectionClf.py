2dcccd20719361e2feb48b57e44bf832d9220f71
Arty_Solomko <artysolomko@gmail.com>
2021-11-20 21:55:59 +0300

    change_set

--- mytools/FeatureSelectionClf.py
+++ mytools/FeatureSelectionClf.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 import shap
 from sklearn.model_selection import train_test_split
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import BaseEstimator,TransformerMixin
 
 
-class FeatureSelectionClf(BaseEstimator, TransformerMixin) :
+class FeatureSelectionClf(BaseEstimator,TransformerMixin):
     """
     Класс вычисляет  важность признаков на основе библиотеки Shap для задачи классификации.
     Работает только с деревьями для большей эффективности или моделями на основе
@@ -73,73 +73,73 @@ class FeatureSelectionClf(BaseEstimator, TransformerMixin) :
 
       """
 
-    def __init__(self, estimator, n_features_to_select = None, columns = None) :
-        self.estimator = estimator
-        self.n_features_to_select = n_features_to_select
-        self.columns = columns
+    def __init__(self,estimator,n_features_to_select=None,columns=None):
+        self.estimator=estimator
+        self.n_features_to_select=n_features_to_select
+        self.columns=columns
 
-    def fit(self, X, y, **fit_params) :
+    def fit(self,X,y,**fit_params):
 
-        X_train, self.X_val, y_train, y_val = train_test_split(
-            X, y, test_size = .25, random_state = 42
+        X_train,self.X_val,y_train,y_val=train_test_split(
+            X,y,test_size=.25,random_state=42
             )
 
-        assert X_train.shape[0] + self.X_val.shape[0] == X.shape[0], 'Неправильно разделена выборка'
+        assert X_train.shape[0]+self.X_val.shape[0]==X.shape[0],'Неправильно разделена выборка'
 
-        self.estimator.fit(X_train, y_train)
+        self.estimator.fit(X_train,y_train)
 
-        preds = self.estimator.predict_proba(self.X_val)[:, 1]
-        explainer = shap.TreeExplainer(self.estimator)
-        self.shap_values = explainer.shap_values(self.X_val)
+        preds=self.estimator.predict_proba(self.X_val)[:,1]
+        explainer=shap.TreeExplainer(self.estimator)
+        self.shap_values=explainer.shap_values(self.X_val)
 
-        try :
+        try:
 
-            vals = np.abs(self.shap_values).mean(0)
-            self.feature_importance = pd.DataFrame(
-                list(zip(self.columns, vals)),
-                columns = ['col_name', 'feature_importance_vals']
+            vals=np.abs(self.shap_values).mean(0)
+            self.feature_importance=pd.DataFrame(
+                list(zip(self.columns,vals)),
+                columns=['col_name','feature_importance_vals']
                 )
             self.feature_importance.sort_values(
-                by = ['feature_importance_vals'],
-                ascending = False, inplace = True
+                by=['feature_importance_vals'],
+                ascending=False,inplace=True
                 )
-            self.idx = (list(self.feature_importance['feature_importance_vals'].index)
+            self.idx=(list(self.feature_importance['feature_importance_vals'].index)
             [:self.n_features_to_select]
             )
 
-        except :
+        except:
 
-            vals = np.abs(self.shap_values[:1]).mean(0)
-            self.feature_importance = pd.DataFrame(
-                list(zip(self.columns, vals)),
-                columns = ['col_name', 'feature_importance_vals']
+            vals=np.abs(self.shap_values[:1]).mean(0)
+            self.feature_importance=pd.DataFrame(
+                list(zip(self.columns,vals)),
+                columns=['col_name','feature_importance_vals']
                 )
             self.feature_importance.sort_values(
-                by = ['feature_importance_vals'],
-                ascending = False, inplace = True
+                by=['feature_importance_vals'],
+                ascending=False,inplace=True
                 )
-            self.idx = (list(self.feature_importance['feature_importance_vals'].index)
+            self.idx=(list(self.feature_importance['feature_importance_vals'].index)
             [:self.n_features_to_select]
             )
 
         return self
 
-    def transform(self, X, y = None) :
-        try :
-            X_selected = X[:, self.idx]
-        except :
-            X_selected = X.iloc[:, self.idx]
+    def transform(self,X,y=None):
+        try:
+            X_selected=X[:,self.idx]
+        except:
+            X_selected=X.iloc[:,self.idx]
         return X_selected
 
-    def get_index(self) :
+    def get_index(self):
         return self.idx
 
-    def plot_values(self) :
-        return shap.summary_plot(self.shap_values, self.X_val)
+    def plot_values(self):
+        return shap.summary_plot(self.shap_values,self.X_val)
 
     @property
-    def _estimator_type(self) :
+    def _estimator_type(self):
         return self.estimator._estimator_type
 
-    def get_feature_importance(self) :
+    def get_feature_importance(self):
         return self.feature_importance
--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from os import path
 from setuptools import setup
 
 try:
-    from featureselectors import __about__ as about
+    from mytools import __about__ as about
 except ImportError:
     
-    sys.path.append("featureselectors")
+    sys.path.append("mytools")
     import __about__ as about
 
 PATH_HERE = path.abspath(path.dirname(__file__))
@@ -18,13 +18,13 @@ with open(path.join(PATH_HERE, 'requirements.txt'), encoding='utf-8') as fp:
 
 
 setup(
-    name='featureselectors',
+    name='mytools',
     version=about.__version__,
     url=about.__homepage__,
     author=about.__author__,
     author_email=about.__author_email__,
     description=about.__doc__,
-    packages=['featureselectors'],
+    packages=['mytools'],
     keywords='shap,fi,pipeline',
     install_requires=requirements,
     python_requires='>=3.6',
