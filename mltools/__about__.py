07fa96c8b98457f19a0b9b4ffaa9a49070055cc1
Arty_Solomko <artysolomko@gmail.com>
2021-11-20 19:57:51 +0300

    add_setup

---
+++ mltools/__about__.py
@@ -0,0 +1,14 @@
+__version__ = "0.0.1"
+__author__ = "Artem, et al."
+__author_email__ = "artysolomko@gmail.com"
+__homepage__ = "https://github.com/ArtyKrafty/featureselectors",
+__doc__ = "Rласс вычисляет важность признаков на основе библиотеки Shap для задачи классификации" \
+          " Работает только с деревьями для большей эффективности или моделями на основе градиентного бустинга"
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__doc__",
+    "__homepage__",
+    "__version__",
+]
---
+++ mltools/setup.py
@@ -0,0 +1,34 @@
+
+import sys
+from os import path
+
+from setuptools import setup
+
+PATH_HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(PATH_HERE, 'requirements.txt'), encoding='utf-8') as fp:
+    requirements = [rq.rstrip() for rq in fp.readlines() if not rq.startswith('#')]
+
+
+setup(
+    name='featureselectors',
+    version=about.__version__,
+    url=about.__homepage__,
+    author=about.__author__,
+    author_email=about.__author_email__,
+    description=about.__doc__,
+    packages=['featureselectors'],
+    keywords='shap,fi,pipeline',
+    install_requires=requirements,
+    python_requires='>=3.6',
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Console',
+        'Natural Language :: English',
+
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
