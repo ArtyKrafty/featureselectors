88d57613529851f28648419d30ff917878790d0b
Artem Solomko <80192450+ArtyKrafty@users.noreply.github.com>
2021-11-20 21:12:58 +0300

    Create __init__.py

---
+++ featureselectors/__init__.py
@@ -0,0 +1,10 @@
+from featureselectors.__about__ import * 
+from featureselectors.featureselectors import FeatureSelectionClf
+from featureselectors.featureselectors import FeatureSelectionRegression
+from featureselectors.featureselectors import FeatureSelectionUniversal
+
+__all__ = [
+    'FeatureSelectionUniversal',
+    'FeatureSelectionRegression',
+    'FeatureSelectionUniversal'
+]
